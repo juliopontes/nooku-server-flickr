79cdd839caae8cd9d53350ef906fe46fdd86fcff
Julio Pontes <juliopfneto@gmail.com>
2011-09-08 16:54:27 -0300

    check vars on template

--- views/person/tmpl/info.php
+++ views/person/tmpl/info.php
@@ -1,5 +1,7 @@
 <?php defined('KOOWA') or die('Restricted access'); ?>
-<a target="_blank" href="<?= $person->profileurl->_content; ?>"><?= $person->realname->_content; ?></a>
-<?= $person->location->_content; ?>
-<br />
-<?= $person->photos->count->_content; ?> photos
+<?php if(is_object($person)): ?>
+	<a target="_blank" href="<?= $person->profileurl->_content; ?>"><?= $person->realname->_content; ?></a>
+	<?= $person->location->_content; ?>
+	<br />
+	<?= $person->photos->count->_content; ?> photos
+<?php endif; ?>
--- views/tags/tmpl/cloud.php
+++ views/tags/tmpl/cloud.php
@@ -1,6 +1,8 @@
 <?php defined('KOOWA') or die('Restricted access'); ?>
-<ul>
-<?php foreach($tags as $tag): ?>
-	<li><?php echo $tag->_content; ?></li>
-<?php endforeach; ?>
-</ul>
+<?php if(!empty($tags)): ?>
+	<ul>
+	<?php foreach($tags as $tag): ?>
+		<li><?php echo $tag->_content; ?></li>
+	<?php endforeach; ?>
+	</ul>
+<?php endif; ?>
